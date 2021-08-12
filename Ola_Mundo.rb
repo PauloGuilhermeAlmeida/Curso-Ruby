b5bc82bbdf780978614e0b0603ffb0bc6a5d98cd
Paulo Guilherme Almeida <88797958+PauloGuilhermeAlmeida@users.noreply.github.com>
2021-08-12 10:36:27 -0300

    Add files via upload

---
+++ Ola_Mundo.rb
@@ -0,0 +1,36 @@
+=begin Informacoes
+    *****************************************
+    *             Curso de Ruby             *
+    *****************************************
+    *   Aluno.....: Paulo Guilherme         *
+    *   Linguagem.: Ruby                    *
+    *   Arquivo...: Ola_Mundo.rb            *
+    *   Aula......: 01                      *
+    *   Data......: 11 de Agosto de 2021    *
+    *****************************************
+=end
+
+#Comando P
+puts "\n\nUsando o comando \"p\""
+puts "Comandos: \np \"Ola \" \np \"Mundo\" \np \"!\"\n\nResultado:\n"
+p "Ola "
+p "Mundo"
+p "!"
+
+#Comando Puts
+puts "\n\nUsando o comando \"puts\""
+puts "Comandos: \nputs \"Ola \" \nputs \"Mundo\" \nputs \"!\"\n\nResultado:\n"
+puts "Ola "
+puts "Mundo"
+puts '!'
+
+#Comando Print
+puts "\n\nUsando o comando \"print\""
+puts "Comandos: \nprint \"Ola \" \nprint \"Mundo\" \nprint \"!\"\n\nResultado:\n"
+print "Ola "
+print "Mundo"
+print '!'
+
+puts "\n\n\n"   #Imprime tres linhas em branco
+
+#Fim do codigo
